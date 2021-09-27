2fc6ae9ca21bf1474a477f643b5ce04cfa3697da
d-ava <d.avalishvili@gmail.com>
2021-09-28 01:58:19 +0400

    e-mail check is on. added if statement on button listener.
    gradient background was added to the layouts
    
    text view added to login layout. USER INFORMATION.
    
    thats it for now

--- app/src/main/java/com/example/homework_005/LoginActivity.kt
+++ app/src/main/java/com/example/homework_005/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.homework_005
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework_005.databinding.ActivityLoginBinding
@@ -10,6 +11,7 @@ class LoginActivity : AppCompatActivity() {
 
 
 
+    @SuppressLint("SetTextI18n") //wtf? check later
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,9 +20,11 @@ class LoginActivity : AppCompatActivity() {
 
         val email = intent.getStringExtra("email")
         val password = intent.getStringExtra("password")
+        val emailString = getText(R.string.email_info)
+        val passwordString = getString(R.string.password_info)
 
-        binding.tVemailInfo.text = "email = $email"
-        binding.tVPasswordInfo.text = "password = $password"
+        binding.tVemailInfo.text ="$emailString  $email "
+        binding.tVPasswordInfo.text = "$passwordString  $password"
     }
 
 
--- app/src/main/java/com/example/homework_005/MainActivity.kt
+++ app/src/main/java/com/example/homework_005/MainActivity.kt
@@ -2,33 +2,48 @@ package com.example.homework_005
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.example.homework_005.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-         setContentView(binding.root)
-
+        setContentView(binding.root)
 
 
         binding.btnLogin.setOnClickListener {
+
             val email: String = binding.eTemail.text.toString()
             val password: String = binding.eTpassword.text.toString()
 
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
 
-             intent.putExtra("email", email)
-             intent.putExtra("password", password)
 
-            startActivity(intent)  }
+
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.eTemail.text.toString()).matches()){
+                    val intent = Intent(this@MainActivity, LoginActivity::class.java)
+
+                    intent.putExtra("email", email)
+                    intent.putExtra("password", password)
+
+                    startActivity(intent)
+
+                }else {binding.eTemail.error = getText(R.string.wrongEmail)
+
+                binding.btnLogin.text = getText(R.string.wrongEmail)
+                }
+
+
+        }
 
 
     }
