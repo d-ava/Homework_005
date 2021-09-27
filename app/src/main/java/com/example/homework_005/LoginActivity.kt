0b372c16ed0bed7d7a11e5589a0d89eb0c71c5df
d-ava <d.avalishvili@gmail.com>
2021-09-27 22:48:15 +0400

    navigate between Main and Login activities. using intent. put data in intent. get data in Login activity  and set it to the text view

--- app/src/main/java/com/example/homework_005/LoginActivity.kt
+++ app/src/main/java/com/example/homework_005/LoginActivity.kt
@@ -2,12 +2,34 @@ package com.example.homework_005
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.homework_005.databinding.ActivityLoginBinding
 
-class LoginActivity :AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    //private lateinit var binding: ActivityLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //binding = ActivityLoginBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+        //setContentView(R.layout.activity_login)
+
+
+        //val intent = intent
+
+       // var email = intent.getStringExtra("email")
+       // var password = intent.getStringExtra("password")
+
+        // findViewById<TextView>(R.id.tVinfo).text = "email $email and password $password"
+
+        val email = intent.getStringExtra("email")
+        val password = intent.getStringExtra("password")
+
+        binding.tVinfo.text = "email $email and password $password"
     }
 
 
--- app/src/main/java/com/example/homework_005/MainActivity.kt
+++ app/src/main/java/com/example/homework_005/MainActivity.kt
@@ -1,16 +1,52 @@
 package com.example.homework_005
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.homework_005.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    // private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        //   binding = ActivityMainBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+
+        //setContentView(R.layout.activity_main)
+
+
+  /*      val loginButton = findViewById<Button>(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+           // val email = findViewById<EditText>(R.id.eTemail).text.toString()
+            //val password = findViewById<EditText>(R.id.eTpassword).text.toString()
+
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            //intent.putExtra("email", email)
+            //intent.putExtra("password", password)
+            startActivity(intent)
+        }*/
+
+        binding.btnLogin.setOnClickListener {
+            val email: String = binding.eTemail.text.toString()
+            val password: String = binding.eTpassword.text.toString()
+
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+
+             intent.putExtra("email", email)
+             intent.putExtra("password", password)
+
+            startActivity(intent)  }
+
+
     }
+
+
 }
