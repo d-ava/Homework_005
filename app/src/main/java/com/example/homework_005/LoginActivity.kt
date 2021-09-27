4a432c4f59d41ef916c67a03567354fcba1a5afb
d-ava <d.avalishvili@gmail.com>
2021-09-27 18:55:27 +0400

    added LoginActivity, activity_main.xml. declared in manifest

---
+++ app/src/main/java/com/example/homework_005/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.homework_005
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+
+}
